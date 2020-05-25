ea7307b1c82a95eb22d18b0752761b95ecbafd17
Marlon Gonzalez <marlongonzalezdev@gmail.com>
2020-05-25 00:40:35 -0300

    01-Define Architecture

---
+++ SocialMedia.Api/Controllers/PostController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SocialMedia.Infrastructure.Repositories;
+
+namespace SocialMedia.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult GetPost()
+        {
+            var posts = new PostRepository().GetPosts();
+            return Ok(posts);
+        }
+    }
+}
---
+++ SocialMedia.Infrastructure/Repositories/PostRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SocialMedia.Core.Entities;
+
+namespace SocialMedia.Infrastructure.Repositories
+{
+    public class PostRepository
+    {
+        public IEnumerable<Post> GetPosts()
+        {
+            var posts = Enumerable.Range(1, 10).Select(x => new Post
+            {
+                Id = x,
+                Description = $"Description {x}",
+                Date= DateTime.Now,
+                Image = $"https://missapis.com/{x}",
+                UserId =  x*2 
+            });
+
+            return posts;
+        }
+    }
+}
